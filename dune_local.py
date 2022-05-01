9184076f30061897b5e3062e4cb3f5f3f14001a6
defifunk <defifunk@gmail.com>
2022-05-01 17:01:40 +0800

    muted params

--- dune_local.py
+++ dune_local.py
@@ -13,9 +13,9 @@ def fetch_records(dune: DuneAPI) -> list[DuneRecord]:
         name=f"BMC Ultraminers - Unclaimed HASH (last updated: {datetime_val} UTC)",
         network=Network.MAINNET,
         parameters=[
-            QueryParameter.number_type("IntParam", 10),
-            QueryParameter.date_type("DateParam", datetime(2022, 3, 10, 12, 30, 30)),
-            QueryParameter.text_type("TextParam", "aba"),
+            # QueryParameter.number_type("IntParam", 10),
+            # QueryParameter.date_type("DateParam", datetime(2022, 3, 10, 12, 30, 30)),
+            # QueryParameter.text_type("TextParam", "aba"),
         ],
     )
     return dune.fetch(sample_query)
